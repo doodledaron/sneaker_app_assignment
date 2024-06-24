bd6a3a82f6d469a95b5e3457bb5551f7160ae202
Tang Yu Xuan <67047462+doodledaron@users.noreply.github.com>
2024-06-24 14:10:22 +0800

    added whitenoise

--- sneaker_app/settings.py
+++ sneaker_app/settings.py
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 # ]
